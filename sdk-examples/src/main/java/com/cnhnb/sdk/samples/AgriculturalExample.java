60c787b4c6b20c5ffc38498d1721b643b7cfb3a9
dragonlong1986 <dragonlong1986@126.com>
2020-06-29 11:54:23 +0800

    增加一个example

---
+++ sdk-examples/src/main/java/com/cnhnb/sdk/samples/AgriculturalExample.java
@@ -0,0 +1,13 @@
+package com.cnhnb.sdk.samples;
+
+public class AgriculturalExample {
+
+  public static void main(String[] args) {
+    //设置需要操作的账号的AK和SK
+    String ACCESS_KEY = "DWQOcrnPp1ogwgAHBdIK1mI-";
+    String SECRET_KEY = "cJFhYuaq7Vo35e1pmXO0aGkJG";
+
+    System.out.println("ACCESS_KEY is " + ACCESS_KEY + ", SECRET_KEY is " + SECRET_KEY);
+
+  }
+}
